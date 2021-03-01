ca6f09461bf50fdd6fc4497ced2df5ac8f34a9a2
unknown <vishalpwaman123@gmail.com>
2021-03-01 15:01:35 +0530

    Added: Add Menu Feature

--- src/Component/Screen.jsx
+++ src/Component/Screen.jsx
@@ -15,62 +15,74 @@ import ChatItem from '../Component/ChatItem'
 import '../Component/ChatContent.scss';
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft'
 import ChatMenuOption from '../Component/ChatMenuOption'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SmartphoneIcon from '@material-ui/icons/Smartphone';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import HourglassEmptyOutlinedIcon from '@material-ui/icons/HourglassEmptyOutlined';
+import RedeemIcon from '@material-ui/icons/Redeem';
+import InvertColorsIcon from '@material-ui/icons/InvertColors';
+import GTranslateOutlinedIcon from '@material-ui/icons/GTranslateOutlined';
 
 export default class Screen extends React.Component {
 
     messagesEndReference = React.createRef(null);
- 
-      constructor(props) {
+
+    constructor(props) {
         super(props);
         this.state = {
-          chat: this.chatItms,
-          Chatmenu : this.ChatMenuButton,
-          msg: "",
-          min : 9,
-          Max : 100000000,
+            chat: this.chatItms,
+            Chatmenu: this.ChatMenuButton,
+            anchorEl: null,
+            msg: "",
+            min: 9,
+            Max: 100000000,
         };
-      }
+    }
 
-      chatItms = [
+    chatItms = [
         {
-          key: 1,
-          type: "",
-          msg: "Hi Abhishek Sir, How are you?",
+            key: 1,
+            type: "",
+            msg: "Hi Abhishek Sir, How are you?",
         },
         {
-          key: 2,
-          type: "other",
-          msg: "I am fine. Vishal",
+            key: 2,
+            type: "other",
+            msg: "I am fine. Vishal",
         },
         {
-          key: 3,
-          type: "other",
-          msg: "What about you?",
+            key: 3,
+            type: "other",
+            msg: "What about you?",
         },
         {
-          key: 4,
-          type: "",
-          msg: "Awesome Abhishek Sir.",
+            key: 4,
+            type: "",
+            msg: "Awesome Abhishek Sir.",
         },
         {
-          key: 5,
-          type: "other",
-          msg: "How Chat System Going On?",
+            key: 5,
+            type: "other",
+            msg: "How Chat System Going On?",
         },
         {
-          key: 6,
-          type: "",
-          msg: "Very Well Sir.",
+            key: 6,
+            type: "",
+            msg: "Very Well Sir.",
         },
         {
-          key: 7,
-          type: "other",
-          msg: "Keep It up",
+            key: 7,
+            type: "other",
+            msg: "Keep It up",
         },
-      ];
+    ];
 
-      ChatMenuButton = [
-        {   
+    ChatMenuButton = [
+        {
             key: 1,
             type: "Fusion Tops"
         },
@@ -84,33 +96,42 @@ export default class Screen extends React.Component {
         },
     ];
 
-      scrollToBottom = () => {
-          this.messagesEndReference.current.scrollIntoView({ behavior: "smooth" });
-      }
+    scrollToBottom = () => {
+        this.messagesEndReference.current.scrollIntoView({ behavior: "smooth" });
+    }
 
-      componentDidMount() {
-          console.log("Track 1");
-          window.addEventListener("keydown",(event) => {
-              if(event.keyCode === 13){
-                  console.log("Track 2",event.keyCode);
-                    if (this.state.msg !== "") {
-                        this.chatItms.push({
-                            key: this.state.min + Math.random() * (this.state.max - this.state.min),
-                            type: "",
-                            msg: this.state.msg,
-                        });
-                        this.setState({ chat: [...this.chatItms]});
-                        this.scrollToBottom();
-                        this.setState({ msg: ""});
-                    }
-              }
-          });
-          this.scrollToBottom();
-      }
+    componentDidMount() {
+        console.log("Track 1");
+        window.addEventListener("keydown", (event) => {
+            if (event.keyCode === 13) {
+                console.log("Track 2", event.keyCode);
+                if (this.state.msg !== "") {
+                    this.chatItms.push({
+                        key: this.state.min + Math.random() * (this.state.max - this.state.min),
+                        type: "",
+                        msg: this.state.msg,
+                    });
+                    this.setState({ chat: [...this.chatItms] });
+                    this.scrollToBottom();
+                    this.setState({ msg: "" });
+                }
+            }
+        });
+        this.scrollToBottom();
+    }
 
-      onStateChange = (event) => {
-          this.setState({ msg: event.target.value});
-      };
+    onStateChange = (event) => {
+        this.setState({ msg: event.target.value });
+    };
+
+    handleClick = (event) => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
 
     render() {
         return (
@@ -143,7 +164,7 @@ export default class Screen extends React.Component {
                                 {this.state.chat.map((itm, index) => {
                                     return (
                                         <ChatItem
-                                            key= {itm.key}
+                                            key={itm.key}
                                             user={itm.type ? itm.type : "me"}
                                             msg={itm.msg}
                                         />
@@ -154,27 +175,170 @@ export default class Screen extends React.Component {
                         </div>
                         <div className="mainContainer_ButtonBody">
                             <div className="mainContainer_ButtonBody_Back">
-                            <Button className="mainContainer_ButtonBody_BackButton"><ArrowLeftIcon fontSize="small"/>Back</Button>
+                                <Button className="mainContainer_ButtonBody_BackButton"><ArrowLeftIcon fontSize="small" />Back</Button>
                             </div>
                             <div className="mainContainer_ButtonBody_OptionMenu" >
-                                
-                            <div className="chat__MenuOption">
-                                {this.state.Chatmenu.map((itm) => {
-                                    return (
-                                        <ChatMenuOption
-                                            key={itm.key}
-                                            type={itm.type}
-                                        />
-                                    );
-                                })}
-                            </div>
+
+                                <div className="chat__MenuOption">
+                                    {this.state.Chatmenu.map((itm) => {
+                                        return (
+                                            <ChatMenuOption
+                                                key={itm.key}
+                                                type={itm.type}
+                                            />
+                                        );
+                                    })}
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="FooterContainer">
                         <div className="subFooterContainer">
                             <div className="subFooterContainer-Button">
-                                <Button><MenuIcon style={{ color: 'white' }} /></Button>
+                                <Button
+                                    aria-controls="subFooterContainer-Buttonmenu"
+                                    aria-haspopup="true"
+                                    onClick={this.handleClick}>
+                                    <MenuIcon style={{ color: 'white' }} />
+                                </Button>
+                                <Menu
+                                    className="subFooterContainer-menuButton"
+                                    id="simple-menu"
+                                    anchorEl={this.state.anchorEl}
+                                    keepMounted
+                                    open={Boolean(this.state.anchorEl)}
+                                    onClose={this.handleClose}
+                                >
+                                    <div className="subFooterContainer-Menu">
+                                        <div className="subFooterContainer-Menu1">
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image1">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton1">
+                                                        <StorefrontIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">Change Store</div>
+                                            </MenuItem>
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image2">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton2">
+                                                        <SmartphoneIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">Change Mobile</div>
+                                            </MenuItem>
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image3">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton3">
+                                                        <TrendingUpIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">Visit Booking</div>
+                                            </MenuItem>
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image4">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton4">
+                                                        <ListAltIcon className="ListAltIcon" />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">My Orders</div>
+                                            </MenuItem>
+                                        </div>
+                                        <div className="subFooterContainer-Menu1">
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image5">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton5">
+                                                        <ShoppingCartOutlinedIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">Shopping Cart</div>
+                                            </MenuItem>
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image6">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton6">
+                                                        <ListAltIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">Digital Reciept</div>
+                                            </MenuItem>
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image7">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton7">
+                                                        <HourglassEmptyOutlinedIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">My Rewards</div>
+                                            </MenuItem>
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image8">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton8">
+                                                        <RedeemIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu1-Text">More Offers</div>
+                                            </MenuItem>
+                                        </div>
+                                        <div className="hrLine"><hr></hr></div>
+                                        <div className="subFooterContainer-Menu2">
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image9">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton9">
+                                                        <InvertColorsIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu2-Text">Change Color</div>
+                                            </MenuItem>
+                                            <MenuItem onClick={this.handleClose}>
+                                                <div className="subFooterContainer-Menu1-Image10">
+                                                    <IconButton
+                                                        color="primary"
+                                                        aria-label="upload picture"
+                                                        component="span"
+                                                        className="IconButton10">
+                                                        <GTranslateOutlinedIcon />
+                                                    </IconButton>
+                                                </div>
+                                                <div className="subFooterContainer-Menu2-Text">Change Languagae</div>
+                                            </MenuItem>
+                                        </div>
+                                    </div>
+                                </Menu>
                             </div>
                             <div className="subFooterContainer-Input">
                                 <FormControl variant="outlined">
